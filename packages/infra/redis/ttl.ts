feccab12943b71d2e4f61839ccb23e6710f80c5b
Aman <amanaziz2020@gmail.com>
2026-01-04 18:08:43 +0530

    feat[redis]: added TTL for relevant operations

---
+++ packages/infra/redis/ttl.ts
@@ -0,0 +1,9 @@
+export const TTL = {
+    STT: 120,                 
+    INTENT: 180,             
+    MEETING_BUFFER: 3600,    
+    CACHE_SHORT: 600,        
+    CACHE_LONG: 1800,        
+    LOCK: 15,                
+  };
+  
