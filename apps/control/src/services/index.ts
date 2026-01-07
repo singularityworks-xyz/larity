1209e5ecc53eac6bb5b420a22d7bb0ce653b55e7
Aman <132681496+tutankhAman@users.noreply.github.com>
2026-01-07 18:25:35 +0530

    feat(control): export services for decisions, meetings, organizations, tasks, and users

---
+++ apps/control/src/services/index.ts
@@ -0,0 +1,5 @@
+export { DecisionService } from './decision.service';
+export { MeetingService } from './meeting.service';
+export { OrgService } from './org.service';
+export { TaskService } from './task.service';
+export { UserService } from './user.service';
