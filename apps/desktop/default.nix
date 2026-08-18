4ac2301134d5a18900d39b0bb9d054d195ae588c
Aman <132681496+tutankhAman@users.noreply.github.com>
2026-08-18 21:11:29 +0530

    ci(images): print deploy API response body for debugging

--- apps/desktop/default.nix
+++ apps/desktop/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "larity";
-  version = "0.0.26";
+  version = "0.0.27";
 
   src = pkgs.fetchurl {
     url = "https://github.com/singularityworks-xyz/larity/releases/download/v${version}/larity_${version}_amd64.deb";
