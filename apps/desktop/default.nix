0c1c2eea39f6a9e69ef9c84ca59d8b4038d95267
Aman <132681496+tutankhAman@users.noreply.github.com>
2026-08-18 21:32:42 +0530

    feat(deploy): pull_policy always so deploys fetch new GHCR images

--- apps/desktop/default.nix
+++ apps/desktop/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "larity";
-  version = "0.0.29";
+  version = "0.0.30";
 
   src = pkgs.fetchurl {
     url = "https://github.com/singularityworks-xyz/larity/releases/download/v${version}/larity_${version}_amd64.deb";
